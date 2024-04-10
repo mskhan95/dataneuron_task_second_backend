e7a82cd8b1f29d67df056799d7199a313a2009bb
mskhan1995 <shabhankhan123456@gmail.com>
2024-04-10 13:44:16 +0530

    Change in Index file

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 const postRouter = require("./Routes/post.route");
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/", postRouter);
